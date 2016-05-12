6031b3d0b508147f450f641955d3bfaee6c54a5d
Justin Hanley <jhanley@arrowheadcu.org>
2016-05-12 11:24:44 -0700

    Add table sorting and styling functionality via dataTable JS library

--- static/helpdesk/custom.js
+++ static/helpdesk/custom.js
@@ -9,3 +9,25 @@ $(function() {
 //$("input[type=date]").each(function() {
 //    if  (this.type != 'date' ) $(this).datepicker();
 //});
+
+
+
+
+//Sortable Tables
+$(document).ready(function() {
+    var table = $('table.sort-table').DataTable( {
+		searching: false,
+		paging: false,
+		info: false,
+		bAutoWidth: false,	
+
+	});
+     
+    $('table.sort-table tbody')
+        .on( 'mouseenter', 'td, th', function () {
+            var colIdx = table.cell(this).index().column;
+ 
+            $( table.cells().nodes() ).removeClass( 'highlight' );
+            $( table.column( colIdx ).nodes() ).addClass( 'highlight' );
+        } );
+} );
